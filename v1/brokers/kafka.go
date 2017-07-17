f80fd4f18262ee6e163d15bde60c70947c86743a
Gleb <glebstepanov1992@gmail.com>
2017-07-17 16:52:03 +0300

    Add kafka cluster Implement consumin partially

--- v1/brokers/kafka.go
+++ v1/brokers/kafka.go
@@ -2,50 +2,92 @@ package brokers
 
 import (
 	"github.com/RichardKnop/machinery/v1/log"
-	"github.com/Shopify/sarama"
+	"github.com/bsm/sarama-cluster"
 	"machinery/v1/tasks"
 )
 
 type KafkaBroker struct {
 	Broker
-	kafkaClient sarama.AsyncProducer
+	consumer      *cluster.Consumer
+	stopReceiving chan struct{}
 }
 
-func NewKafkaBroker(kafkaTopic string, kafkaBrokers []string) (*KafkaBroker, error) {
-	config := sarama.NewConfig()
-	config.Producer.Partitioner = sarama.NewRoundRobinPartitioner
-	log.INFO.Printf("Connecting to kafka brokers %s", kafkaBrokers)
-	client, err := sarama.NewAsyncProducer(kafkaBrokers, config)
+func NewKafkaBroker(kafkaTopics, kafkaBrokers []string, groupId string, offset int64) (*KafkaBroker, error) {
+	config := cluster.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Group.Return.Notifications = true
+
+	log.INFO.Printf("Connecting to kafka brokers %s topics %s", kafkaBrokers, kafkaTopics)
+	consumer, err := cluster.NewConsumer(kafkaBrokers, groupId, kafkaTopics, config)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &KafkaBroker{
-		kafkaClient: client,
+		consumer:      consumer,
+		stopReceiving: make(chan struct{}),
 	}, nil
 }
 
-func (kafka KafkaBroker) SetRegisteredTaskNames(names []string) {
+func (broker *KafkaBroker) SetRegisteredTaskNames(names []string) {
 
 }
 
-func (kafka KafkaBroker) IsTaskRegistered(name string) bool {
+func (broker *KafkaBroker) IsTaskRegistered(name string) bool {
 	return false
 }
 
-func (kafka KafkaBroker) StartConsuming(consumerTag string, p TaskProcessor) (bool, error) {
+func (broker KafkaBroker) StartConsuming(consumerTag string, p TaskProcessor) (bool, error) {
+	go func() {
+		for {
+			select {
+			case err := <-broker.consumer.Errors():
+				log.ERROR.Print(err)
+			case <-broker.stopReceiving:
+				return
+			}
+		}
+	}()
+
+	go func() { // subscribe on notifications chanel
+		for {
+			select {
+			case note := <-broker.consumer.Notifications():
+				log.WARNING.Printf("Rebalanced: %+v\n", note)
+			case <-broker.stopReceiving:
+				return
+			}
+		}
+	}()
+
+	go func() { // push messages to the buffer
+		for {
+			select {
+			case msg := <-broker.consumer.Messages():
+				log.INFO.Print(msg)
+			case broker.stopReceiving:
+				return
+			}
+		}
+	}()
+
 	return false, nil
 }
 
-func (kafka KafkaBroker) StopConsuming() {
+func (broker *KafkaBroker) StopConsuming() {
+	if err := broker.consumer.Close(); err != nil {
+		log.ERROR.Print(err)
+	}
+
+	broker.Broker.stopConsuming()
 
 }
 
-func (kafka KafkaBroker) Publish(task *tasks.Signature) error {
+func (broker *KafkaBroker) Publish(task *tasks.Signature) error {
 	return nil
 }
 
-func (kafka KafkaBroker) GetPendingTasks(queue string) ([]*tasks.Signature, error) {
+func (broker *KafkaBroker) GetPendingTasks(queue string) ([]*tasks.Signature, error) {
 	return nil, nil
 }
